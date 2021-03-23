fc9275b64b2b6b3e5ded299632bbf1c5b35e8e58
rysk1013 <rysk2908@gmail.com>
2021-03-24 02:09:37 +0900

    userのfactorybot設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {'ユーザー'}
+    email {'user@email.com'}
+    password {'111aaa'}
+    password_confirmation {password}
+    profile {'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト'}
   end
 end
