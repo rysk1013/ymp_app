bfdcc28def5a8e94d8ce385ae6afff82e036ad24
rysk1013 <rysk2908@gmail.com>
2021-03-24 02:32:32 +0900

    postのfactorybot設定

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :post do
-    
+    title {'aaaaaaタイトル'}
+    overview {'テストテストテストテストテストテストテストテストテストテストテストtesttesttest'}
+    programming_languages {'HTML, CSS, Ruby, Ruby on Rails'}
+    techs {'AWS, Docker, CircleCI/CD'}
+    portfolio_url {Faker::Internet.url(host: 'example.com', path: '/foobar.html')}
+    github_url {Faker::Internet.url(host: 'example.com', path: '/foobar.html')}
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/sample5.jpeg'), filename: 'sample5.jpeg')
+    end
   end
 end
