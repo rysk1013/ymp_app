e28ff8a79627f572382c8cfb425751fd928aa1e1
rysk1013 <rysk2908@gmail.com>
2021-03-24 03:18:55 +0900

    commentモデルのテストコード設定

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  
+  describe 'コメント機能' do
+    context 'コメントできるとき' do
+      it 'text,user,postが存在すればコメントできること' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントできないとき' do
+      it 'textが空ではコメントできないこと' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
 end
